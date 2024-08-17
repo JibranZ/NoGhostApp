eab3fb47877ecabdd4a868f66d5961948f26d8c5
alitquan <alitmallick.gmail.com>
2024-08-17 03:16:17 -0400

    added footer skeleton

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react' 
+import styles from '../components/Home.module.css'
+
+export default function Footer () {
+	return (
+		<div className= {`${styles.footer}`} > 
+
+			<div className= {`${styles.footerPortion}`} > 
+			</div>
+
+			<div className= {`${styles.footerPortion}`} > 
+			</div>
+		</div>
+	)
+};
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,14 +3,14 @@ import styles from './Home.module.css'
 import Navbar from './Navbar.jsx'
 import Headline from './Headline.jsx'
 import Features from './Features.jsx'
-
+import Footer from './Footer.jsx' 
 export default function Home () {
 	return (
 		<div className={`${styles.base} ${styles.container}`} > 
   			<Navbar />	
 			<Headline /> 
 			<Features /> 
-
+			<Footer /> 
 
 		</div> 
 
